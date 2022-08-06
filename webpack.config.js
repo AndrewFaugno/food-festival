c96865d277b4731a383f61ae3ec1d20322d56e24
Andrew Faugno <andrewfaugno825@gmail.com>
2022-08-05 20:47:18 -0400

    added image loader to webpack config to decrease image sizes

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,50 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
     entry: {
-        app: './assets/js/script.js',
-        events: './assets/js/events.js',
-        schedule: './assets/js/schedule.js',
-        tickets: './assets/js/tickets.js'
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return "[path][name].[ext]";
+                            },
+                            publicPath: function (url) {
+                                return url.replace("../", "/assets/");
+                            },
+                        },
+                    },
+                    {
+                        loader: "image-webpack-loader",
+                    },
+                ],
+            },
+        ],
     },
     output: {
-        filename: '[name].bundle.js',
-        path: __dirname + '/dist'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
     },
     plugins: [
         new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
+            $: "jquery",
+            jQuery: "jquery",
         }),
         new BundleAnalyzerPlugin({
-            analyzerMode: 'static', // the report outputs to an html file in the dist folder
+            analyzerMode: "static", // the report outputs to an html file in the dist folder
         }),
     ],
-    mode: 'development'
+    mode: "development",
 };
